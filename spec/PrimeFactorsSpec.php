d88544b3b39c7b458984d73f420214b41ccebefb
Richard Forbes <richard@evocate.org>
2016-06-01 18:57:29 +0000

    return empty array

--- spec/PrimeFactorsSpec.php
+++ spec/PrimeFactorsSpec.php
@@ -5,7 +5,12 @@ use Prophecy\Argument;
 
 class PrimeFactorsSpec extends ObjectBehavior {
 
-	public function is_it_working(){
-		$this->execute();
+	// public function is_it_working(){
+	// 	$this->execute();
+	// }
+
+	function it_returns_an_empty_array_for_1()
+	{
+		$this->generate(1)->shouldReturn([]);
 	}
 }
